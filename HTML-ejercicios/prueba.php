312a78affc922337592f3950a9bbcf93f122cfda
Lianji10 <jfernandezpuchol@gmail.com>
2025-09-09 19:57:32 +0200

    Add files via upload

---
+++ HTML-ejercicios/prueba.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Primera prueba php</title>
+    </head>
+    <body>
+    Este es un archivo php que se encuentra en el servidor.
+    </body>
+</html>
